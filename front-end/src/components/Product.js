c70f4f4a8f516a31e531ac07c49d9758ff662d95
Rama-Qandeel <rama.qndeel@gmail.com>
2021-02-05 19:53:23 +0200

    create rating with stars

--- front-end/src/components/Product.js
+++ front-end/src/components/Product.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import { propTypes } from "react-bootstrap/esm/Image";
+import Rating from './Rating'
+
 const Product = ({ product }) => {
   return (
     <div>
@@ -15,9 +18,13 @@ const Product = ({ product }) => {
               </Card.Title>
         </a>
         <Card.Text as="div">
-<div className="my-3">
+{/* <div className="my-3">
 {product.rating} from {product.numReviews} reviews
-</div>
+</div> */}
+<Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
         </Card.Text>
         <Card.Text as="h3">
 
