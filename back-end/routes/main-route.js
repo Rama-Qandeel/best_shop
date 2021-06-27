4a50a89a73fcee5f871e8211f7d7cd9daf034e0f
Rama-Qandeel <rama.qndeel@gmail.com>
2021-06-27 13:04:19 +0300

    create route

--- back-end/routes/main-route.js
+++ back-end/routes/main-route.js
@@ -1,8 +1,20 @@
+import asyncHandler from 'express-async-handler'
 import express from 'express';
 const mainRouter = express.Router();
+import Product from '../models/product.js'
 
-mainRouter.get('/', (req, res) => {
-  res.json('HELLO WORLD');
-});
+mainRouter.get('/product', asyncHandler(async (req, res) => {
+  const products = await Product.find({})
+  res.json(products);
+}));
 
+mainRouter.get('/product/:id', asyncHandler(async (req, res) => {
+  const products = await Product.findById(req.params.id)
+  console.log('**********',products)
+  if(products){
+    res.json(products);
+  } else {
+    res.status(404).json({ message : 'product not found'})
+  }
+}));
 export default mainRouter;
