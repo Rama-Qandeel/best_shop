68126d268da68c183b11b774359d144c5bfcc8d4
Rama-Qandeel <rama.qndeel@gmail.com>
2021-07-06 17:47:59 +0300

    add cart functionality

--- front-end/src/store.js
+++ front-end/src/store.js
@@ -2,12 +2,19 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import {productListReducer, productDetailsReducer} from './redusers/product'
+import {cartListReducer} from './redusers/cart'
+
 const reducer = combineReducers({
 productList: productListReducer,
 productDetails: productDetailsReducer, 
+cart: cartListReducer,
 })
 
-const initialState = {}
+const getItems = localStorage.getItem('cartItems')? JSON.parse(localStorage.getItem('cartItems')):[]
+
+const initialState = {
+  cart : {cartItems : []}
+}
 
 const middleware = [thunk]
 
