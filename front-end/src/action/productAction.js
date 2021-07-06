8c2a4466fcefb946a363869dab640bdee6b0d50c
Rama-Qandeel <rama.qndeel@gmail.com>
2021-07-06 15:06:50 +0300

    Fetch product details

--- front-end/src/action/productAction.js
+++ front-end/src/action/productAction.js
@@ -3,8 +3,10 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
 } from '../constant/productConstant'
-
 
 
 export const listProducts = () => async (
@@ -24,6 +26,30 @@ export const listProducts = () => async (
     } catch (error) {
       dispatch({
         type: PRODUCT_LIST_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      })
+    }
+  }
+
+  export const listProductDetails = (id) => async (
+    dispatch
+  ) => {
+    try {
+      dispatch({ type: PRODUCT_DETAILS_REQUEST })
+      const { data } = await axios.get(
+        `http://localhost:5000/product/${id}`
+      )
+  
+      dispatch({
+        type: PRODUCT_DETAILS_SUCCESS,
+        payload: data,
+      })
+    } catch (error) {
+      dispatch({
+        type: PRODUCT_DETAILS_FAIL,
         payload:
           error.response && error.response.data.message
             ? error.response.data.message
--- front-end/src/screens/ProductScreen.js
+++ front-end/src/screens/ProductScreen.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from "../products"
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import { listProductDetails } from '../action/productAction'
+
+
 const ProductScreen = ({match}) => {
-    // console.log(match.params.id);
-    const product=products.find((p)=>p._id===match.params.id)
-    
+  const dispatch = useDispatch()
+  const productDetails= useSelector(state => state.productDetails)
+  const { loading, error, product} = productDetails
+
+  useEffect(() => {
+    dispatch(listProductDetails(match.params.id))
+   }, [dispatch, match])
+
+
     return (
-        <div>
+        <>
        <Link className='btn btn-light my-3' to='/'>
         Go Back
-      </Link>      
-      <Row>
+      </Link>  
+      {loading? <Loader/> :error? <Message variant='danger'>{error}</Message>:(
+        <Row>
             <Col md={6}>
               <Image src={product.image} alt={product.name} fluid />
             </Col>
@@ -60,8 +72,9 @@ const ProductScreen = ({match}) => {
 </Card>
 </Col>
             </Row>
-
-       </div>
+      )}    
+      
+       </>
     )
 }
 
