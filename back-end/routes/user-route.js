bbfa9be3fa3a5e7621e40419cf9bad5691c396ac
Rama-Qandeel <rama.qndeel@gmail.com>
2021-07-09 14:57:43 +0300

    User registration & password hashing

--- back-end/routes/user-route.js
+++ back-end/routes/user-route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 const mainRouter = express.Router();
 import {protect} from '../middlewares/auth.js'
-import {createUser, geteUser} from '../controllers/user-controller.js'
+import {createUser, geteUser, registerUser} from '../controllers/user-controller.js'
 
 mainRouter.post('/login',createUser);
 mainRouter.get('/profile',protect,geteUser);
+mainRouter.post('/register',registerUser);
 
 
 export default mainRouter;
