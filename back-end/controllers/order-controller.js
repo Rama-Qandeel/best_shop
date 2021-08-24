639edb68a5b4be8607798d9d89cea97c04f5d84c
Rama-Qandeel <rama.qndeel@gmail.com>
2021-08-24 14:51:57 +0300

    create function get order by id

--- back-end/controllers/order-controller.js
+++ back-end/controllers/order-controller.js
@@ -1,38 +1,50 @@
-import asyncHandler from 'express-async-handler'
-import Order from '../models/order.js'
-
+import asyncHandler from "express-async-handler";
+import Order from "../models/order.js";
 
 const addOrderItems = asyncHandler(async (req, res) => {
-    const {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("No order items");
+    return;
+  } else {
+    const order = new Order({
       orderItems,
+      user: req.user._id, // from a token
       shippingAddress,
       paymentMethod,
       itemsPrice,
       taxPrice,
       shippingPrice,
       totalPrice,
-    } = req.body
-  
-    if (orderItems && orderItems.length === 0) {
-     res.status(400)
-      throw new Error('No order items')
-      return
-    } else {
-      const order = new Order({
-        orderItems,
-        user: req.user._id, // from a token
-        shippingAddress,
-        paymentMethod,
-        itemsPrice,
-        taxPrice,
-        shippingPrice,
-        totalPrice,
-      })
-  
-      const createdOrder = await order.save()
-  
-      res.status(201).json(createdOrder)
-    }
-  })
-  
-export default addOrderItems
+    });
+
+    const createdOrder = await order.save();
+
+    res.status(201).json(createdOrder);
+  }
+});
+
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email",
+  );
+  console.log("order-------------", order);
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+export  { addOrderItems, getOrderById };
--- back-end/routes/order-route.js
+++ back-end/routes/order-route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 const mainRouter = express.Router();
 import {protect} from '../middlewares/auth.js'
-import addOrderItems from '../controllers/order-controller.js'
+import {addOrderItems, getOrderById} from '../controllers/order-controller.js'
 
 mainRouter.post('/order',protect,addOrderItems);
+mainRouter.get('/order/:id',getOrderById);
 
 
 export default mainRouter;
