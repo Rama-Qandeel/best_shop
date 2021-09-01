da9073c33f2f94d228a22c39086ebd0e67e0a359
Rama-Qandeel <rama.qndeel@gmail.com>
2021-09-01 14:07:23 +0300

    reset list users

--- front-end/src/redusers/user.js
+++ front-end/src/redusers/user.js
@@ -17,6 +17,7 @@ import {
     USER_LIST_REQUEST,
     USER_LIST_SUCCESS,
     USER_LIST_FAIL,
+    USER_LIST_RESET,
 } from '../constant/userConstant'
 
 
@@ -104,6 +105,8 @@ export const userListReducer = (state = {}, action) => {
         }
       case USER_LIST_FAIL:
         return { loading: false, error: action.payload }
+        case USER_LIST_RESET:
+          return { users : []}
       default:
         return state
     }
