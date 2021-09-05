53d22789d50fab8bddcf7a7a294f92155610ad9b
Rama-Qandeel <rama.qndeel@gmail.com>
2021-09-05 23:05:18 +0300

    admin delete user

--- back-end/controllers/user-controller.js
+++ back-end/controllers/user-controller.js
@@ -94,10 +94,23 @@ const getUsers = (asyncHandler(async(req,res) => {
   res.json(users)
 }))
 
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+
+  if (user) {
+    await user.remove()
+    res.json({ message: 'User removed' })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
 export {
     createUser,
     geteUser,
     registerUser,
     updateUserProfile,
-    getUsers
+    getUsers,
+    deleteUser
 }
--- back-end/routes/user-route.js
+++ back-end/routes/user-route.js
@@ -1,13 +1,14 @@
 import express from 'express';
 const mainRouter = express.Router();
 import {protect, admin} from '../middlewares/auth.js'
-import {createUser, geteUser, registerUser, updateUserProfile, getUsers} from '../controllers/user-controller.js'
+import {createUser, geteUser, registerUser, updateUserProfile, getUsers, deleteUser} from '../controllers/user-controller.js'
 
 mainRouter.post('/login', createUser);
 mainRouter.get('/user/:id', protect, geteUser);
 mainRouter.put('/profile', protect, updateUserProfile);
 mainRouter.post('/register', registerUser);
 mainRouter.get('/users', protect, admin, getUsers);
+mainRouter.delete('/user/:id', protect, admin, deleteUser);
 
 
 
