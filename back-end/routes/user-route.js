6c1fed63698690697cbaf30f656e1d5b4ad3d40b
Rama-Qandeel <rama.qndeel@gmail.com>
2021-09-06 00:28:52 +0300

    admin update user

--- back-end/routes/user-route.js
+++ back-end/routes/user-route.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const mainRouter = express.Router();
 import {protect, admin} from '../middlewares/auth.js'
-import {createUser, geteUser, registerUser, updateUserProfile, getUsers, deleteUser} from '../controllers/user-controller.js'
+import {createUser, geteUser, registerUser, updateUserProfile, getUsers, deleteUser, getUserById, updateUser} from '../controllers/user-controller.js'
 
 mainRouter.post('/login', createUser);
 mainRouter.get('/user/:id', protect, geteUser);
@@ -9,7 +9,8 @@ mainRouter.put('/profile', protect, updateUserProfile);
 mainRouter.post('/register', registerUser);
 mainRouter.get('/users', protect, admin, getUsers);
 mainRouter.delete('/user/:id', protect, admin, deleteUser);
-
+mainRouter.get('/user/:id', protect, admin, getUserById);
+mainRouter.put('/user/:id', protect, admin, updateUser);
 
 
 export default mainRouter;
