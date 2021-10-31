dafc046e209ed503aff0ace1a026f7bfd787a733
Rama-Qandeel <rama.qndeel@gmail.com>
2021-10-31 14:41:15 +0200

    craete product and update it

--- back-end/routes/main-route.js
+++ back-end/routes/main-route.js
@@ -1,10 +1,15 @@
 import express from 'express';
 const mainRouter = express.Router();
 import {protect, admin} from '../middlewares/auth.js'
-import {getProduct, getProductById, deleteProduct} from '../controllers/main-controller.js'
+import {getProduct, getProductById, deleteProduct, createProduct, updateProduct} from '../controllers/main-controller.js'
 
 mainRouter.get('/product',getProduct);
 mainRouter.get('/product/:id', getProductById);
-mainRouter.delete('/product/:id', deleteProduct);
+mainRouter.delete('/product/:id', protect, admin, deleteProduct);
+mainRouter.post('/product', protect, admin, createProduct);
+mainRouter.put('/product/:id', protect, admin, updateProduct);
+
+
+
 
 export default mainRouter;
